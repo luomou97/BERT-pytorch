c2d94a1f0f04ad7242d17813f7a39b9878e07fde
luomou97 <44665299+luomou97@users.noreply.github.com>
2021-06-11 14:17:15 +0800

    Update sublayer.py

--- bert_pytorch/model/utils/sublayer.py
+++ bert_pytorch/model/utils/sublayer.py
@@ -15,4 +15,4 @@ class SublayerConnection(nn.Module):
 
     def forward(self, x, sublayer):
         "Apply residual connection to any sublayer with the same size."
-        return x + self.dropout(sublayer(self.norm(x)))
+        return x + self.dropout(sublayer(self.norm(x)))     # 'norm & add' rather than 'add & norm' 
