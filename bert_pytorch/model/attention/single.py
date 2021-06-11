3d2ce28b2d12c81e4545bb5fd7fab5e6714de66f
luomou97 <44665299+luomou97@users.noreply.github.com>
2021-06-11 14:24:39 +0800

    Update single.py

--- bert_pytorch/model/attention/single.py
+++ bert_pytorch/model/attention/single.py
@@ -12,7 +12,7 @@ class Attention(nn.Module):
 
     def forward(self, query, key, value, mask=None, dropout=None):
         scores = torch.matmul(query, key.transpose(-2, -1)) \
-                 / math.sqrt(query.size(-1))
+                 / math.sqrt(query.size(-1))    # matmul(<batch_size, len_(Q/K), dim>, <batch_size, dim, len_(Q/K)>) => <batch_size, len_(Q/K), len_(Q/K)>
 
         if mask is not None:
             scores = scores.masked_fill(mask == 0, -1e9)
